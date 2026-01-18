5f1c29efa7d8332d9826d12a8e0d4439dda5938d
BlackCoconut <gal.vilmos.91@gmail.com>
2026-01-18 13:17:37 +0100

    Nem vált át 20-ról 21-re.

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert('Hello');
+//alert('Hello');
+
+/* console.log('Hello')
+console.error('Hiba van');
+console.warn('Lehet hiba');
+ */
+// -- Változók
+
+//let, const, var
+let kor = 20;
+kor = 21;
+console.log(kor);
