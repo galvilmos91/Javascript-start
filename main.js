119b575d6fb8a9cd356cec061bc2e188ba5979db
BlackCoconut <gal.vilmos.91@gmail.com>
2026-01-19 18:07:55 +0100

    nem ertem

--- main.js
+++ main.js
@@ -4,9 +4,36 @@
 console.error('Hiba van');
 console.warn('Lehet hiba');
  */
+
 // -- Változók
 
 //let, const, var
-const kor = 20;
-//kor = 21;
-console.log(kor);
+var kor = 20;
+kor = 21;
+//console.log(kor);
+
+
+// -- Adat típusok
+
+const nev = 'Vilmos'; //String
+const kor2 =21; // Number
+const suly = 100; // Number
+const felnott = true; // Boolean
+const date = new Date('2020-10-01');
+
+const x = null;
+let y;
+
+//console.log(typeof y);
+
+// -- Műveletek
+
+let szam1 = 10;
+szam1 += '1';
+
+let nev2 = 'Vilmos';
+/* nev2 += 10; */
+
+let szoveg = 'Hello';
+
+console.log('${szoveg} ${nev2}!!!!');
